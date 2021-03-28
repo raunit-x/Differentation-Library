4abc16fd811bd6ef5151b817fec47401de99da81
Raunit Dalal <raunit88@gmail.com>
2021-03-28 23:43:18 +0530

    main program fixes

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from lexer import Lexer
 from parser_ import Parser
 from interpreter import Interpreter
+from lexer import MATH_FUNCTIONS
 
 
-while True:
-    text = input("Expr > ")
-    lexer = Lexer(text, {'z', 'y'})
-    tokens = list(lexer.generate_tokens())
-    print(f"Tokens: {tokens}")
-    parser = Parser(tokens)
-    tree = parser.parse()
-    # print(f"Tree: {tree}")
-    interpreter = Interpreter()
-    value = interpreter.visit(tree)
-    print(f"Value: {value}")
+if __name__ == '__main__':
+    print("SYMBOLIC DIFFERENTIATION CALCULATOR")
+    print(f"ALLOWED MATH FUNCTIONS: {MATH_FUNCTIONS}")
+    print(f"SYNTAX: cos(expression), OPERATIONS: [+, -, *, /]")
+    while True:
+        text = input("Expr > ")
+        lexer = Lexer(text, {'z', 'y'})
+        tokens = list(lexer.generate_tokens())
+        print(f"Tokens: {tokens}")
+        parser = Parser(tokens)
+        tree = parser.parse()
+        interpreter = Interpreter()
+        value = interpreter.visit(tree)
+        print(f"Value: {value}")
